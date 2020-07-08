82203e36bc5ebecebb4f960f5e2919bb475638a3
allen_yu <allenyu@kingwaytek.com>
2020-07-08 17:23:39 +0800

    remote other unuse themes

--- themes/jacman/source/js/redirect2https.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    if (window.location.protocol != 'https:' && window.location.hostname.indexOf("localhost") < 0) {
-        location.href = location.href.replace("http://", "https://");
-    }
-};
